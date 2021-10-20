aa5cee1ccdffb37b6d177f58fa81fe97968cac9a
Santiago Franco <ktns0930@gmail.com>
2021-10-19 23:58:51 -0500

    Code finished.

--- src/main/java/com/example/library/controllers/ResourceController.java
+++ src/main/java/com/example/library/controllers/ResourceController.java
@@ -3,6 +3,7 @@ package com.example.library.controllers;
 import com.example.library.DTOs.ResourceDTO;
 import com.example.library.collections.Resource;
 import com.example.library.services.interfaces.IResourceService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,5 +46,30 @@ public class ResourceController {
     @PutMapping(value = "/update")
     public ResponseEntity<ResourceDTO> updateResource(@RequestBody ResourceDTO resourceDTO){
         return new ResponseEntity<>(service.update(resourceDTO), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/avaible/{id}")
+    public ResponseEntity<String> resourceAvaibilityValidation(@PathVariable String id){
+        return new ResponseEntity<>(service.checkResourceAvailability(id),HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/lend/{id}")
+    public ResponseEntity<String> lendResource(@PathVariable String id){
+        return new ResponseEntity<>(service.lendRecourse(id), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/return/{id}")
+    public ResponseEntity<String> returnResource(@PathVariable String id){
+        return new ResponseEntity<>(service.returnResource(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/type/{kind}")
+    public ResponseEntity<List<ResourceDTO>> recommendedByType(@PathVariable String kind){
+        return new ResponseEntity<>(service.recommendByResourceType(kind), HttpStatus.OK);
+    }
+
+    @GetMapping("/thematic/{thematic}")
+    public ResponseEntity<List<ResourceDTO>> recommendedByThematic(@PathVariable String thematic){
+        return new ResponseEntity<>(service.recommendByTheme(thematic), HttpStatus.OK);
     }
 }
