4c1fe65aa87faa026a8040b4e048786a29bb6ef4
Santiago Franco <ktns0930@gmail.com>
2021-10-19 22:55:48 -0500

    createResource has been created in controller.

--- src/main/java/com/example/library/controllers/ResourceController.java
+++ src/main/java/com/example/library/controllers/ResourceController.java
@@ -6,10 +6,8 @@ import com.example.library.services.interfaces.IResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.converter.feed.RssChannelHttpMessageConverter;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,10 @@ public class ResourceController {
         return service.getById(id)
                 .map(resourceDTO -> new ResponseEntity<>(resourceDTO, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<ResourceDTO> createResource(@RequestBody ResourceDTO resourceDTO){
+        return new ResponseEntity<>(service.create(resourceDTO), HttpStatus.CREATED);
     }
 }
