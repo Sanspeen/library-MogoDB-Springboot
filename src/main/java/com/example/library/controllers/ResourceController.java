c1f3c2fe9374e545df497ebf4c5ca19b3d54e2ba
Santiago Franco <ktns0930@gmail.com>
2021-10-19 22:53:08 -0500

    getResourceById Has been added.

--- src/main/java/com/example/library/controllers/ResourceController.java
+++ src/main/java/com/example/library/controllers/ResourceController.java
@@ -4,7 +4,10 @@ import com.example.library.DTOs.ResourceDTO;
 import com.example.library.collections.Resource;
 import com.example.library.services.interfaces.IResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +22,12 @@ public class ResourceController {
     @GetMapping
     List<ResourceDTO> listResources(){
         return service.list();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ResourceDTO> getResourceById(@PathVariable() String id){
+        return service.getById(id)
+                .map(resourceDTO -> new ResponseEntity<>(resourceDTO, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
